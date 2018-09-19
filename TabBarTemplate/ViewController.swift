5bd6851b3c8e7c45ba15c017c37bba7dbf673afe
Joe Ward <joethermal@gmail.com>
2018-09-19 15:45:08 +1200

    Initial Commit

---
+++ TabBarTemplate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarTemplate
+//
+//  Created by Joseph Ward on 19/09/18.
+//  Copyright © 2018 Joethermal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
